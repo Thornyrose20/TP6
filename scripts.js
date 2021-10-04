47a5d43ca43b7cdaff98b387ec76bea4dfa6e7d3
Thornyrose20 <nokurose@hotmail.com>
2021-10-04 22:19:17 +0000

    Added code to hide the li's

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-//hidding list items underneath ingredients,equipment and directions
+//hiding list items underneath ingredients,equipment and directions
+
+// hiding ingredients
+window.onload = hiddenIngredients();{
+  document.getElementById('Ing').style.display ='none';
+}
+
+//hiding equipment
+window.onload = hiddenEquipment();{
+  document.getElementById('Equip').style.display ='none';
+}
+
+//hiding directions
+window.onload = hiddenDirections(); {
+  document.getElementById('Dir').style.display = 'none';
+}
