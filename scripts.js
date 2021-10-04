696f219be7f33245b433290d6d73f172884fd23e
Thornyrose20 <nokurose@hotmail.com>
2021-10-04 22:34:30 +0000

    added equipment and direction code

--- scripts.js
+++ scripts.js
@@ -18,8 +18,29 @@ window.onload = hiddenDirections(); {
 // making the lists visable on webpage
 
 //making list button work
-function hiddenIngredients(){
+// Ingredients button
+function hiddenIngredients() {
   var y = document.getElementById("Ing");
+  if(y.style.display === "none"){
+    y.style.display = "block";
+  } else{
+    y.style.display ="none";
+  }
+}
+
+//Equipment button
+  function hiddenEquipment() {
+  var y = document.getElementById("Equip");
+  if(y.style.display === "none"){
+    y.style.display = "block";
+  } else{
+    y.style.display ="none";
+  }
+}
+
+// direction button
+function hiddenDirections() {
+  var y = document.getElementById("Dir");
   if(y.style.display === "none"){
     y.style.display = "block";
   } else{
