8cace92d2a7ad81ccd483ac949a7d11c78743a0b
Thornyrose20 <nokurose@hotmail.com>
2021-10-06 21:18:21 +0000

    changed code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
-//hiding list items underneath ingredients,equipment and directions
+//hiding list items underneath ingredients,equipment and directions and toggling them
 
+// toggle the ingredient box
+window.onload = function() {
+  document.querySelector("#box1").onclick = function (){
+    document.querySelector("#box1 ul").classList.toggle("showMe");
+  }
+  
+  //toggle the equipment box
+ window.onload = function(){
+   document.querySelector("#box2").onclick = function(){
+     document.querySelector("box2 ul").classList.toggle("showMe");
+   }
+ } 
+  
+  
+  //toggle the directions box
+  window.onload = function(){
+    document.querySelector("#box3").onclick = function(){
+      document.querySelector("#box3 ul").classList.toggle("ShowMe");
+    }
+  }
+}
